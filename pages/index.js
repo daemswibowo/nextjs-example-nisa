444e80a238c159cb3d3545b78eddcedf9bf40610
Dimas Wibowo <daemswibowo@gmail.com>
2020-11-13 16:30:57 +0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,35 @@
+import React, {useState} from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [state, setState] = useState({
+        task: '',
+        tasks: [],
+    });
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    function handleSubmit(e) {
+        e.preventDefault();
+        let tasks = state.tasks;
+        tasks.push(state.task);
+        setState((s) => ({...s, tasks}));
+    }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    return (
+        <div id={"todoapp"} style={{height: '100vh'}}>
+            <div className="todoContainer">
+                <h1>Todo App</h1>
+                <form noValidate={true} onSubmit={handleSubmit}>
+                    <p>Task name</p>
+                    <input type="text" placeholder={'Example: Mandi'} onChange={({target}) => setState((s) => ({...s, task: target.value}))}/>
+                    <button type="submit">Create</button>
+                </form>
+                <h1>Task List</h1>
+                {state.tasks.length === 0 && "No task"}
+                <ul>
+                    {state.tasks.map((task, key) => <li key={key}>{task}</li>)}
+                </ul>
+            </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
